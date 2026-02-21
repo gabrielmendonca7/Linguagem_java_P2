054ffb9fdeeac0d931b7deeb1ef466215c0b6c2c
Gabriel <gabrielmito054@gmail.com>
2026-02-21 15:40:18 -0300

    Primeiro exercício da aula do dia 19/02 já realizado.

---
+++ src/desafios/ex_01.java
@@ -0,0 +1,18 @@
+package desafios;
+
+/*
+Faça um programa que calcule o salário de um funcionário com
+base no valor da hora trabalhada de R$ 15.00 e na quantidade de
+40 horas trabalhadas e exiba o salário.
+ */
+
+public class ex_01 {
+    public static void main(String[] args){
+
+        double valor_por_hora = 15;
+        double hora_trabalhada = 40;
+        double salario = valor_por_hora * hora_trabalhada;
+
+        System.out.printf("O seu salário no período foi de: %.2f", salario);
+    }
+}
