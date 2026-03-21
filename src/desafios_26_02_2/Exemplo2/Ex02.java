aaf927bdbe3363bc1e61938543ef00c784bc3610
Gabriel <gabrielmito054@gmail.com>
2026-03-21 19:21:06 -0300

    Realizada a segunda atividade da aula do dia 26/02.

---
+++ src/desafios_26_02_2/Exemplo2/Ex02.java
@@ -0,0 +1,26 @@
+package desafios_26_02_2.Exemplo2;
+
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Sistema sistema = new Sistema();
+
+        System.out.printf("Digite seu nome: ");
+        sistema.setNome(sc.nextLine());
+
+        System.out.printf("Quantas notas deseja inserir: ");
+        int quantidade = sc.nextInt();
+
+        double[] notasDigitadas = new double[quantidade];
+
+        for (int i = 0; i < quantidade; i++){
+            System.out.printf("Digite sua nota (%d): ",i+1);
+            notasDigitadas[i] = sc.nextDouble();
+        }
+
+        sistema.statusAcademico(notasDigitadas);
+    }
+}
---
+++ src/desafios_26_02_2/Exemplo2/Sistema.java
@@ -0,0 +1,39 @@
+package desafios_26_02_2.Exemplo2;
+
+import java.util.Arrays;
+
+public class Sistema{
+    private String nome;
+    private double[] notas;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNotas(double[] notas){
+        this.notas = notas;
+    }
+    public double[] getNotas(){
+        return notas;
+    }
+
+    public void statusAcademico(double[] notas){
+        double media = Arrays.stream(notas).average().orElse(0);
+
+        if (media >= 70)
+        {
+            System.out.printf("O aluno %s está aprovado!",getNome());
+        }
+        else if (media < 40)
+        {
+            System.out.printf("O aluno %s está reprovado!",getNome());
+        }
+        else
+        {
+            System.out.printf("O aluno %s está na final!",getNome());
+        }
+    }
+}
