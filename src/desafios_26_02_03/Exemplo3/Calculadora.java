8123a4f275c552046fd6af2c93f96679924c69f3
Gabriel <gabrielmito054@gmail.com>
2026-03-28 19:26:16 -0300

    Realizada a terceira atividade da aula do dia 26/02.

---
+++ src/desafios_26_02_03/Exemplo3/Calculadora.java
@@ -0,0 +1,69 @@
+package desafios_26_02_03.Exemplo3;
+
+public class Calculadora {
+    private int numero1,numero2,opcao;
+
+    public int getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(int numero1) {
+        this.numero1 = numero1;
+    }
+
+    public int getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(int numero2) {
+        this.numero2 = numero2;
+    }
+
+    public int getOpcao() {
+        return opcao;
+    }
+
+    public void setOpcao(int opcao) {
+        this.opcao = opcao;
+    }
+
+    private int soma(int numero1, int numero2){
+       return numero1 + numero2;
+    }
+
+    private int subtracao(int numero1,int numero2){
+        return numero1 - numero2;
+    }
+    private int divisao(int numero1,int numero2){
+        return numero1 / numero2;
+    }
+    private int multiplicacao(int numero1,int numero2){
+        return numero1 * numero2;
+    }
+
+    public void Resultado(){
+
+            switch (opcao){
+                case 1:
+                    System.out.printf("O resuldado da operação é: %d",soma(numero1,numero2));
+                    break;
+                case 2:
+                    System.out.printf("O resuldado da operação é: %d",subtracao(numero1,numero2));
+                    break;
+                case 3:
+                    if(numero1 == 0 || numero2 == 0){
+                        System.out.print("Operação com divisor 0 não é permitido");
+                        break;
+                    }
+                    System.out.printf("O resuldado da operação é: %d",divisao(numero1,numero2));
+                    break;
+                case 4:
+                    System.out.printf("O resuldado da operação é: %d", multiplicacao(numero1,numero2));
+                    break;
+                default:
+                    System.out.print("Operação não consta nas opções.");
+                    break;
+
+        }
+    }
+}
---
+++ src/desafios_26_02_03/Exemplo3/Ex03.java
@@ -0,0 +1,40 @@
+package desafios_26_02_03.Exemplo3;
+
+import java.util.Scanner;
+
+public class Ex03 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        Calculadora calculadora = new Calculadora();
+        boolean rever = true;
+
+        while (rever)  {
+            System.out.printf("Digite uma opção: \n");
+            System.out.printf("1 - SOMA \n");
+            System.out.printf("2 - SUBTRAÇÃO \n");
+            System.out.printf("3 - DIVISÃO \n");
+            System.out.printf("4 - MULTIPLICAÇÃO \n");
+            System.out.printf("0 - REVER OPERAÇÕES \n");
+
+            calculadora.setOpcao(scanner.nextInt());
+
+            System.out.print("Digite o primeiro número: ");
+            calculadora.setNumero1(scanner.nextInt());
+            System.out.print("Digite o segundo número: ");
+            calculadora.setNumero2(scanner.nextInt());
+
+            calculadora.Resultado();
+
+            System.out.print("\nDeseja realizar novo calculo (zero (0) para sim): ");
+            int resposta = scanner.nextInt();
+
+            if (resposta != 0){
+                rever = false;
+                System.out.print("Encerrando...");
+            }
+        }
+        scanner.close();
+}
+}
+
